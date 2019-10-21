d942eb0cd5b691f1254c91a7f0f15078c650640e
godcheese <godcheese@outlook.com>
2019-10-21 09:59:38 +0800

    Update travis.build.sh

--- scripts/travis.build.sh
+++ scripts/travis.build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 java -version
+mysql -V
 chmod +x mvnw
+mvnw --version
 sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('123456') where User='root'; update user set plugin='mysql_native_password';flush privileges;"
 sudo mysql_upgrade -uroot -p123456
 sudo mysql -uroot -p123456 -e "use mysql;create user 'nimrod'@'%';update user set authentication_string=PASSWORD('123456') where User='nimrod';update user set plugin='mysql_native_password';grant usage on *.* to 'nimrod' require none with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0;create database if not exists nimrod;grant all privileges on nimrod.* to 'nimrod'@'%';flush privileges;"
