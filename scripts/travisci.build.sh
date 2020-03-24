6062324f4af5d4a97833b89daa25133a7d2d735a
godcheese <godcheese@outlook.com>
2020-03-24 11:42:04 +0800

    Update travisci.build.sh

--- scripts/travisci.build.sh
+++ scripts/travisci.build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "author godcheese"
 
-yum update -y && yum install -y wget && wget -O install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos/centos7_install_jdk8.sh && sh install.sh 
+   yum update -y && yum install -y wget && wget -O install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos/centos7_install_jdk8.sh && sh install.sh 
 wget -O install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos/centos7_install_maven3.sh && sh install.sh
 wget -O install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos/centos7_install_mysql57.sh && sh install.sh
 sudo mysql -uroot -p123456 -e "use mysql;create user 'nimrod'@'%';update user set authentication_string=PASSWORD('123456') where User='nimrod';update user set plugin='mysql_native_password';grant usage on *.* to 'nimrod' require none with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0;create database if not exists nimrod;grant all privileges on nimrod.* to 'nimrod'@'%';flush privileges;"
