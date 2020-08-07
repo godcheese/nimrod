9e3dc3c1b052ec7e0a30073655a2d277b9ee7bf9
godcheese <godcheese@outlook.com>
2020-08-07 16:37:57 +0800

    Initial commit

--- scripts/travisci.build.sh
+++ scripts/travisci.build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+# encoding: utf-8.0
+
+# http://github.com/godcheese/shell_bag
+# author: godcheese [godcheese@outlook.com]
+# description: gitlabci.build.sh
 
 echo "author godcheese [godcheese@outlook.com]"
-curl -o install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/jdk/centos7_install_jdk8u202.sh && bash install.sh
-curl -o install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/maven/centos7_install_maven363.sh && bash install.sh
-wget -O install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/mysql/centos7_install_mysql5731.sh && bash install.sh
+curl -o install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos7/install_jdk.sh && bash install.sh install /webwork/software/jdk https://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz jdk1.8.0_202
+ curl -o install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos7/install_maven.sh && bash install.sh install /webwork/software/maven https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz apache-maven-3.6.3
+curl -o install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos7/install_mysql.sh && bash install.sh install /webwork/software/mysql https://mirrors.tuna.tsinghua.edu.cn/mysql/downloads/MySQL-5.7/mysql-5.7.31-el7-x86_64.tar.gz mysql-5.7.31-el7-x86_64
 sudo mysql -uroot -p123456 -e "use mysql;create user 'nimrod'@'%';update user set authentication_string=PASSWORD('123456') where User='nimrod';update user set plugin='mysql_native_password';grant usage on *.* to 'nimrod' require none with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0;create database if not exists nimrod;grant all privileges on nimrod.* to 'nimrod'@'%';flush privileges;"
 sudo mysql -unimrod -p123456 nimrod < db/mysql/nimrod/nimrod.sql
 sudo mvn -e clean install -DskipTests=true -Dmaven.javadoc.skip=true -Dspring-boot.run.profiles=dev
