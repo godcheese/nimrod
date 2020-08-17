6e595dd41c0e956d252d29850da7dc9e8956aeff
godcheese <godcheese@outlook.com>
2020-08-17 13:13:34 +0800

    Update travisci.build.sh

--- scripts/travisci.build.sh
+++ scripts/travisci.build.sh
@@ -5,25 +5,41 @@
 # author: godcheese [godcheese@outlook.com]
 # description: travisci.build.sh
 
-echo "author godcheese [godcheese@outlook.com]"
-echo "*********************"
-cat /etc/*-release
-echo "*"
-cat /etc/issue
-echo "*"
-lsb_release -a
-echo "*"
-uname -a
-echo "*"
-uname -mrs
-echo "*"
-man uname
-echo "*"
-cat /proc/version
-echo "*********************"
-sudo curl -o install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos7/install_jdk.sh && sudo bash install.sh install /webwork/software/jdk https://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz jdk1.8.0_202
-sudo curl -o install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos7/install_maven.sh && sudo bash install.sh install /webwork/software/maven https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz apache-maven-3.6.3
-sudo curl -o install.sh https://raw.githubusercontent.com/godcheese/shell_bag/master/centos7/install_mysql.sh && sudo bash install.sh install /webwork/software/mysql https://mirrors.tuna.tsinghua.edu.cn/mysql/downloads/MySQL-5.7/mysql-5.7.31-el7-x86_64.tar.gz mysql-5.7.31-el7-x86_64
+release_id=$(awk '/^NAME="/' /etc/os-release | awk -F '"' '{print $2}' | awk -F ' ' '{print $1}' | tr 'A-Z' 'a-z' 2>&1)
+release_name=
+release_version=$(awk '/^VERSION="/' /etc/os-release | awk -F '"' '{print $2}' | awk -F ' ' '{print $1}' 2>&1)
+release_full_version=
+linux_kernel=$(uname -srm | awk -F ' ' '{print $2}' | awk -F '-' '{print $1}' 2>&1)
+function system_info() {
+  case "${release_id}" in
+  "centos")
+    release_name="CentOS"
+    release_full_version=$(awk '/\W/' /etc/centos-release | awk '{print $4}' 2>&1)
+    ;;
+  "debian")
+    release_name="Debian"
+    release_full_version=$(cat /etc/debian_version 2>&1)
+    ;;
+  "ubuntu")
+    release_name="Ubuntu"
+    release_full_version=${release_version}
+    release_version=$(echo ${release_version} | awk -F '.' '{print $1}')
+    ;;
+  *)
+    echo "Unknown system."
+    exit 0
+    ;;
+  esac
+}
+system_info
+echo "Release Version: ${release_name} ${release_full_version}"
+echo "Kernel Version: ${linux_kernel}"
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y maven
+sudo apt-get install -y mysql-server
+sudo mysql_secure_installation
+systemctl restart mysql.service
 sudo mysql -uroot -p123456 -e "use mysql;create user 'nimrod'@'%';update user set authentication_string=PASSWORD('123456') where User='nimrod';update user set plugin='mysql_native_password';grant usage on *.* to 'nimrod' require none with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0;create database if not exists nimrod;grant all privileges on nimrod.* to 'nimrod'@'%';flush privileges;"
 sudo mysql -unimrod -p123456 nimrod < db/mysql/nimrod/nimrod.sql
 sudo mvn -e clean install -DskipTests=true -Dmaven.javadoc.skip=true -Dspring-boot.run.profiles=dev
